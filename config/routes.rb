2d85e87f8a5d1fc3829d21493a7e4b387c92f435
LaRaye <lnj2105@gmail.com>
2019-07-15 21:57:37 -0400

    signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#logout'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :contributors
   resources :contributions
   resources :candidates
-  resources :users, except: :new
+  resources :users, except: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
