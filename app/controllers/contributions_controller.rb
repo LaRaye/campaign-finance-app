d0572ac4b5caaa5f5b09392b011bd78242919dcf
LaRaye <lnj2105@gmail.com>
2019-07-17 22:51:07 -0400

    contributions crud

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,15 +1,42 @@
 class ContributionsController < ApplicationController
+  def index
+    @contributions = Contribution.all
+  end
+
+  def show
+    find_contribution
+  end
+
   def new
     @contribution = Contribution.new
   end
 
   def create
-    @contribution = Contribution.new
+    @contribution = Contribution.new(contribution_params)
     if @contribution.save
       redirect_to contribution_path(@contribution)
     else
-      render :new 
+      render :new
     end
   end
 
+  def edit
+    find_contribution
+    render :edit
+  end
+
+  def destroy
+    find_contribution.destroy
+    redirect_to contributions_path
+  end
+
+  private
+
+  def find_contribution
+    @contribution = Contribution.find(params[:id])
+  end
+
+  def contribution_params
+    params.require(:contribution).permit(:amount, :date, :contributor_name, :candidate_name)
+  end
 end
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,11 +2,21 @@ class Contribution < ApplicationRecord
   belongs_to :candidate
   belongs_to :contributor
 
-  def candidate_name=(name)
-    self.candidate = Candidate.find_or_create_by(name: name)
+  def candidate_name=(candidate_name)
+    first_name = candidate_name.split(" ")[0]
+    last_name = candidate_name.split(" ")[1]
+    self.candidate = Candidate.find_or_create_by(first_name: first_name, last_name: last_name)
   end
 
   def candidate_name
-    self.candidate ? self.candidate.new : nil
+    self.candidate ? (self.candidate.first_name + " " + self.candidate.last_name) : nil
+  end
+
+  def contributor_name=(contributor_name)
+    self.contributor = Contributor.find_or_create_by(name: contributor_name)
+  end
+
+  def contributor_name
+    self.contributor ? self.contributor.name : nil
   end
 end
