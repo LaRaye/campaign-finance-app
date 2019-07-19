53e8e35a8f5bab2a45c6429ba097320d7f5425c8
LaRaye <lnj2105@gmail.com>
2019-07-19 12:15:33 -0400

    contributor crud

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,2 +1,47 @@
 class ContributorsController < ApplicationController
+  def index
+    @contributors = Contributor.all
+  end
+
+  def show
+    find_contributor
+  end
+
+  def new
+    @candidate = Contributor.new
+  end
+
+  def create
+    @contributor = Contributor.new(contributor_params)
+    if @candidate.save
+      redirect_to contributor_path(@contributor)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find_contributor
+  end
+
+  def update
+    find_contributor
+    @contributor.update(contributor_params)
+    redirect_to contributor_path(@contributor)
+  end
+
+  def destroy
+    find_contributor.destroy
+    redirect_to contributor_path
+  end
+
+  private
+
+  def find_contributor
+    @contributor = Contributor.find(params[:id])
+  end
+
+  def contributor_params
+    params.require(:contributor).permit(:name, :category, :industry_affiliation)
+  end
 end
