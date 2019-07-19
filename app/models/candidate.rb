125ebf90d9fb5f8830e8b70427229267be8937ee
LaRaye <lnj2105@gmail.com>
2019-07-19 15:05:26 -0400

    candidate validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,8 +3,10 @@ class Candidate < ApplicationRecord
   has_many :contributors, through: :contributions
   has_many :favorites, dependent: :destroy
   ##has_many :users, through: :favorites --??? aliases?? favorited_candidates?
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def full_name
-    self.first_name + " " + self.last_name
+      self.first_name + " " + self.last_name
   end
 end
