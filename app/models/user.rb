e3f43e8f3a49a6cc8375d2f1f4cbd52a29d4209f
LaRaye <lnj2105@gmail.com>
2019-07-20 00:44:54 -0400

    validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :candidates, through: :favorites ##aliases??
 
+  validates :username, presence: true, uniqueness: true
   validates :password, length: { in: 6..20 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
