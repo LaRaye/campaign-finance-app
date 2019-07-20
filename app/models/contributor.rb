82815d3ea28e3593266d9aae091e2e112e212f9b
LaRaye <lnj2105@gmail.com>
2019-07-20 00:43:05 -0400

    validations for user and contributor

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,4 +1,6 @@
 class Contributor < ApplicationRecord
   has_many :contributions
-  has_many :candidates, through: :contributions 
+  has_many :candidates, through: :contributions
+
+  validates :name, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :favorites
   has_many :candidates, through: :favorites ##aliases??
+
+  validates :password, length: { in: 6..20 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
