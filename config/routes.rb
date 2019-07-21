bf241d7231dfc267ba4126acf77f560fc09c1443
LaRaye <lnj2105@gmail.com>
2019-07-20 21:24:04 -0400

    facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
+  get '/auth/facebook/callback', to: 'sessions#create'
+
   resources :contributors
   resources :contributions
   resources :candidates
