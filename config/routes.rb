208d4d7d3b93934ec7f3007d7cfde9373c8b949f
LaRaye <lnj2105@gmail.com>
2019-07-21 22:54:09 -0400

    navigation and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
 
   get '/auth/facebook/callback', to: 'sessions#fb_create'
 
