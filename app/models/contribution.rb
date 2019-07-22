ebbc540b431fc097a0d37a8a49b1b947b08274c3
LaRaye <lnj2105@gmail.com>
2019-07-21 20:44:08 -0400

    fix date validation for contributions

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -5,9 +5,16 @@ class Contribution < ApplicationRecord
   after_save :assign_size
 
   validates :amount, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :date, presence: true, format: {with: /\d{2}\/\d{2}\/\d{4}/}
   validates :candidate_name, presence: true
   validates :contributor_name, presence: true
+  validates :date, presence: true, format: {with: /\d{2}\/\d{2}\/\d{4}/}
+  validates_each :date do |record, attr, value|
+    begin
+      Date.parse(value)
+    rescue
+      record.errors.add(attr, "Invalid date")
+    end
+  end
 
   scope :all_by_date, -> { order(date: :desc)}
 
